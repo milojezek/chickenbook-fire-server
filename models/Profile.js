2301cf802ae5505515114a6336e733d92de3180a
Miloslav Jezek <milo.jezek@gmail.com>
2024-07-26 16:06:03 +0200

    Create profile model

---
+++ models/Profile.js
@@ -0,0 +1,12 @@
+class Profile {
+	constructor(id, name, email, calling, imageUrl, skills) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.calling = calling;
+		this.imageUrl = imageUrl;
+		this.skills = skills;
+	}
+}
+
+export default Profile;
