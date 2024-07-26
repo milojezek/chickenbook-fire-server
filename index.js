ad11b5e1391912e3b29d41e5a74415c9d44da118
Miloslav Jezek <milo.jezek@gmail.com>
2024-07-26 16:25:01 +0200

    Add profile routes to index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 
 import config from "./config.js";
+import profileRoutes from "./routes/profileRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api", profileRoutes);
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
